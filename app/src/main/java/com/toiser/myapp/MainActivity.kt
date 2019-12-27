774b3b96cb48fa0d0dd2548432eb7f7d99f3bb1f
YvesToiser <yves.toiser@gmail.com>
2019-12-27 12:39:03 +0100

    fix choose line event remove title bar

--- app/src/main/java/com/toiser/myapp/MainActivity.kt
+++ app/src/main/java/com/toiser/myapp/MainActivity.kt
@@ -12,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Remove Title Bar
+        if(supportActionBar != null) {
+            supportActionBar?.hide()
+        }
 
         // Choose Metro Line Buttons
 
@@ -27,28 +31,24 @@ class MainActivity : AppCompatActivity() {
         btnB.isEnabled = true
 
         // set on-click listeners
-        if (btnA.isEnabled) {
-            btnA.setOnClickListener {
-                btnA.isEnabled = false
-                btnB.isEnabled = true
-                selectedLine =  MetroLineEnum.A
-                btnA.setImageResource(R.drawable.a_active)
-                btnB.setImageResource(R.drawable.b_inactive)
-                spinnerDeparture.setBackgroundResource(R.drawable.spinner_bar_red)
-                spinnerArrival.setBackgroundResource(R.drawable.spinner_bar_red)
-            }
+        btnA.setOnClickListener {
+            btnA.isEnabled = false
+            btnB.isEnabled = true
+            selectedLine =  MetroLineEnum.A
+            btnA.setImageResource(R.drawable.a_active)
+            btnB.setImageResource(R.drawable.b_inactive)
+            spinnerDeparture.setBackgroundResource(R.drawable.spinner_bar_red)
+            spinnerArrival.setBackgroundResource(R.drawable.spinner_bar_red)
         }
 
-        if (btnB.isEnabled) {
-            btnB.setOnClickListener {
-                btnA.isEnabled = true
-                btnB.isEnabled = false
-                selectedLine =  MetroLineEnum.B
-                btnA.setImageResource(R.drawable.a_inactive)
-                btnB.setImageResource(R.drawable.b_active)
-                spinnerDeparture.setBackgroundResource(R.drawable.spinner_bar_yellow)
-                spinnerArrival.setBackgroundResource(R.drawable.spinner_bar_yellow)
-            }
+        btnB.setOnClickListener {
+            btnA.isEnabled = true
+            btnB.isEnabled = false
+            selectedLine =  MetroLineEnum.B
+            btnA.setImageResource(R.drawable.a_inactive)
+            btnB.setImageResource(R.drawable.b_active)
+            spinnerDeparture.setBackgroundResource(R.drawable.spinner_bar_yellow)
+            spinnerArrival.setBackgroundResource(R.drawable.spinner_bar_yellow)
         }
 
     }
